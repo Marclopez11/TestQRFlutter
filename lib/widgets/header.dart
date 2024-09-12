b98d4aa905c16272ce81540ca5e3b4dedd0b0047
Marc Lopez <marclopezmarco469@gmail.com>
2024-09-12 22:45:34 +0200

    idiomas pero no fnciona

--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class Header extends StatelessWidget {
-  const Header({Key? key}) : super(key: key);
+  final bool showLanguageDropdown;
+
+  const Header({Key? key, this.showLanguageDropdown = true}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -9,12 +11,50 @@ class Header extends StatelessWidget {
       height: 60,
       color: Theme.of(context).scaffoldBackgroundColor,
       padding: EdgeInsets.symmetric(horizontal: 16),
-      child: Center(
-        child: Image.asset(
-          'assets/images/logo_felanitx.png',
-          height: 40,
-        ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Image.asset(
+            'assets/images/logo_felanitx.png',
+            height: 40,
+          ),
+          if (showLanguageDropdown)
+            LanguageDropdown(), // Utiliza el widget LanguageDropdown
+          if (!showLanguageDropdown)
+            Spacer(), // Añade un espacio flexible a la derecha si no se muestra el dropdown
+        ],
       ),
+    );
+  }
+}
+
+class LanguageDropdown extends StatefulWidget {
+  const LanguageDropdown({Key? key}) : super(key: key);
+
+  @override
+  _LanguageDropdownState createState() => _LanguageDropdownState();
+}
+
+class _LanguageDropdownState extends State<LanguageDropdown> {
+  String _selectedLanguage = 'ES';
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: _selectedLanguage,
+      onChanged: (String? newValue) {
+        setState(() {
+          _selectedLanguage = newValue!;
+        });
+      },
+      items: <String>['ES', 'EN', 'CA', 'DE', 'FR'].map((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+      underline: Container(),
+      icon: Icon(Icons.arrow_drop_down),
     );
   }
 }
